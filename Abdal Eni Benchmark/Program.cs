b33166755fa3591433685a8d2758df5101b65993
abdal-security-group <abdal.group@gmail.comgit>
2023-09-23 00:33:15 +0330

    Abdal Eni Benchmark

---
+++ Abdal Eni Benchmark/Program.cs
@@ -0,0 +1,154 @@
+﻿using Abdal_Eni_Benchmark;
+using System;
+using System.Collections.Concurrent;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static int successfulRequests = 0;
+    private static int failedRequests = 0;
+
+    static async Task Main(string[] args)
+    {
+        Version version = Assembly.GetExecutingAssembly().GetName().Version;
+
+        Console.Title = "Abdal Eni Benchmark " + version.Major + "." + version.Minor;
+        Banner.banner();
+
+        Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+        Console.WriteLine("Usage: Abdal Eni Benchmark.exe  <url> <connectionCount> <requestPerConnection> <totalDurationInSeconds>");
+        Console.WriteLine("Description: A web performance benchmarking tool with customizable connections and requests.");
+        Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        try
+        {
+            if (args.Length == 4)
+            {
+                await RunBenchmark(args[0], int.Parse(args[1]), int.Parse(args[2]), int.Parse(args[3]));
+            }
+            else
+            {
+                await RunBenchmark();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error occurred but continuing: {ex.Message}");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static async Task RunBenchmark(string? serverUrl = null, int connectionCount = 0, int requestPerConnection = 0, int totalDurationInSeconds = 0)
+    {
+        if (serverUrl == null)
+        {
+            Console.WriteLine("Enter the server URL:");
+            serverUrl = Console.ReadLine();
+        }
+
+        if (connectionCount == 0)
+        {
+            Console.WriteLine("Enter the number of connections:");
+            if (!int.TryParse(Console.ReadLine(), out connectionCount))
+            {
+                Console.WriteLine("Invalid input. Exiting.");
+                return;
+            }
+        }
+
+        if (requestPerConnection == 0)
+        {
+            Console.WriteLine("Enter the number of requests per connection:");
+            if (!int.TryParse(Console.ReadLine(), out requestPerConnection))
+            {
+                Console.WriteLine("Invalid input. Exiting.");
+                return;
+            }
+        }
+
+        if (totalDurationInSeconds == 0)
+        {
+            Console.WriteLine("Enter the total duration in seconds:");
+            if (!int.TryParse(Console.ReadLine(), out totalDurationInSeconds))
+            {
+                Console.WriteLine("Invalid input. Exiting.");
+                return;
+            }
+        }
+
+        var httpClient = new HttpClient();
+        var tasks = new ConcurrentBag<Task>();
+
+        Console.WriteLine("Waiting...");
+
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(totalDurationInSeconds));
+
+        for (int i = 0; i < connectionCount; i++)
+        {
+            tasks.Add(MakeRequests(httpClient, serverUrl, requestPerConnection, cts.Token));
+        }
+
+        try
+        {
+            await Task.WhenAll(tasks);
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Benchmark operation was cancelled after the specified duration.");
+        }
+
+       
+
+        // Displaying the summary at the end
+        Console.WriteLine("---------------------------------------------------");
+        Console.WriteLine($"Successful requests: {successfulRequests}");
+        Console.WriteLine($"Failed requests: {failedRequests}");
+        Console.WriteLine("# Benchmark Summary:");
+        Console.WriteLine($"Server URL: {serverUrl}");
+        Console.WriteLine($"Number of Connections: {connectionCount}");
+        Console.WriteLine($"Requests per Connection: {requestPerConnection}");
+        Console.WriteLine($"Total Duration (seconds): {totalDurationInSeconds}");
+        Console.WriteLine("---------------------------------------------------");
+    }
+
+    static async Task MakeRequests(HttpClient httpClient, string? serverUrl, int requestPerConnection, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(serverUrl))
+        {
+            failedRequests += requestPerConnection;
+            return;
+        }
+
+        for (int i = 0; i < requestPerConnection; i++)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            try
+            {
+                var response = await httpClient.GetAsync(serverUrl, cancellationToken);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    successfulRequests++;
+                }
+                else
+                {
+                    failedRequests++;
+                }
+            }
+            catch
+            {
+                failedRequests++;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# Abdal-Eni-Benchmark
+# Abdal Eni Benchmark
+
+![](https://raw.githubusercontent.com/ebrasha/abdal-4iproto-server-config/main/shot.jpg)
+
+# 🤵 Programmer
+Ebrahim Shafiei (EbraSha)
+
+Email :  Prof.Shafiei@Gmail.com
+
+## README Translation
+- [English](README.md)
+- [فارسی](README.fa.md)
+
+## 💎 General purpose
 A web performance benchmarking tool with customizable connections and requests.
+
+## 💎 Description
+"Abdal Eni Benchmark" is a robust tool designed for benchmarking and testing the performance of websites and web services. With this tool, users can specify the number of connections, the number of requests sent per connection, and the test execution duration. Test results are displayed in real-time.
+
+# ✨ Features
+- Without any malicious code and spyware
+- Concurrent requests with adjustable connection count.
+- Ability to specify the number of requests sent per connection.
+- Set benchmark execution duration.
+- Real-time display of results with counts of successful and failed requests.
+- User-friendly text interface for easy use.
+- Error handling with uninterrupted operations.
+- Run with parameters or direct user input.
+- Completely open source
+
+
+If, you are interested in free projects and helping the people of the world, send a message so that we can build a better world together._
+_
+
+# 💡 Installation
+
+#### For all operating systems
+Just download and run the version you desire.
+ 
+## ✅ Supported operating systems
+
+- debian.11-x64
+- debian.11-arm64
+- debian.10-x64
+- ubuntu.22.04-x64
+- ubuntu.22.04-arm64
+- ubuntu.20.04-x64
+- ubuntu.18.04-x64
+- centos.7-x64
+- rhel.7-x64
+- rhel.8-x64
+- fedora.37-x64
+
+ 
+
+## ⚠️ Legal disclaimer ⚠️
+
+Use this tool only for legal purposes and with permission. The developer assumes no liability for any illicit usage or harm inflicted upon others as a result of using this tool.
+
+## ❤️ Donations to the project
+
+> USDT:      TKRmTvwkMFdgGrwvDHztLXsKAwK2WEEKUp
+
+> bitcoin:   bc1q9w9ymgz2wluax60rsuza4q0at7gpy82g8el6zj
+
+> For Iranian People -> MellatBank : 6104-3378-5301-4247
+
+## ❗ Reporting Issues
+
+If you are facing a configuration issue or something is not working as you expected to be, please use the Prof.Shafiei@Gmail.com . Issues on GitLab are also welcomed.
+
